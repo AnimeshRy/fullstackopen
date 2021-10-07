cf2529199a990c2e6c9cf73bf6a6547c2d1b6db9
AnimeshRy <ani16september@gmail.com>
2021-10-07 18:40:42 +0530

    ex 9.1 - 9.7 done

---
+++ part9/starting-typescript/index.ts
@@ -0,0 +1,66 @@
+import express from 'express';
+import { parseBmiArguments, calculateBmi } from './bmicalculator';
+import {
+    parseExerciseArguments,
+    exerciseCalculator
+} from './exerciseCalculator';
+
+const app = express();
+app.use(express.json());
+
+app.get('/', (_req, res) => {
+    res.send('Hello Full Stack!');
+});
+
+app.get('/bmi', (req, res) => {
+    const weight = req.query.weight;
+    const height = req.query.height;
+
+    if (!weight || !height) {
+        res.status(400);
+        res.send({ error: 'missing parameter height or weight' });
+    } else {
+        try {
+            const { heightInCm, weightInKg } = parseBmiArguments(
+                Number(height),
+                Number(weight)
+            );
+            const bmi = calculateBmi(heightInCm, weightInKg);
+            res.send({
+                weight: weightInKg,
+                height: heightInCm,
+                bmi: bmi
+            });
+        } catch (e) {
+            res.status(400);
+            res.send({ error: e });
+        }
+    }
+});
+
+app.post('/exercises', (req, res) => {
+    const dailyExercises = req.body.daily_exercises;
+    const dailyTarget = req.body.target;
+
+    if (!dailyExercises || !dailyTarget) {
+        res.status(400);
+        res.send({ error: 'missing parameter daily_exercises or target' });
+    } else {
+        try {
+            const { target, dailyExerciseHours } = parseExerciseArguments(
+                dailyTarget,
+                dailyExercises
+            );
+            res.send(exerciseCalculator(target, dailyExerciseHours));
+        } catch (e) {
+            res.status(400);
+            res.send({ error: e });
+        }
+    }
+});
+
+const PORT = 3003;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
